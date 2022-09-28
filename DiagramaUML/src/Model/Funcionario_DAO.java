1fca34a8782e1ce71f710da1cf876961847819d1
Dodoh04 <douglasoliversa3@gmail.com>
2022-09-27 22:10:14 -0300

    imagem e exercicio dois

---
+++ DiagramaUML/src/Model/Funcionario_DAO.java
@@ -0,0 +1,12 @@
+
+package Model;
+
+public class Funcionario_DAO {
+    String Nome = null;
+    String Endereco = null;
+    String email = null;
+    int Telefone = 0;
+    public static void provarExistencia(){
+        
+    } 
+}
